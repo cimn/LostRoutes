c5ba4617b13fb96bf42b153755e0ad52583b4daf
cimn <cimn123123@gmail.com>
2015-12-15 20:25:11 +0800

    bulletdone

--- src/Bullet.js
+++ src/Bullet.js
@@ -14,13 +14,42 @@ var Bullet = new cc.PhysicsSprite.extend({
 
         this.shape = new cp.BoxShape(this.body,this.getContentSize().width,this.getContentSize().height);
         shape.setElasticity(0.5);
-        shape.setFriction(0.);
+        shape.setFriction(0.5);
         shape.setCollisionType(Collision_Type.Bullet);
         this.space.addShape(shape);
         this.setBody(this.body);
         this.body.data = this;
     },
     shootBulletFromFighter: function(p){
-
+        this.body.data = this;
+        this.body.setPos(p);
+        this.scheduleUpdate();
+    },
+    update: function(dt){
+        //计算移动距离
+        this.body.setPos(cc.p(this.body.getPos().x + this.velocity.x * dt,this.body.getPos().y + this.velocity.y * dt));
+        if(this.body.getPos().y > winSize.height){
+            this.unscheduleUpdate();
+            this.body.data = null;
+            this.removeFromParent();
+        }
+    },
+    unuse: function(){
+        this.retain();  //if i jsb
+        this.setVisible(false);
+    },
+    reuse: function(spriteFrameName,space){
+        this.spriteFrameName = spriteFrameName;
+        this.space = space;
+        this.setVisible(true);
     }
-})
+});
+
+
+Bullet.create = function(spriteFrameName,space){
+    if(cc.pool.hasObject(Bullet)) {
+        return cc.pool.getFromPool(Bullet, spriteFrameName, space);
+    } else {
+        return new Bullet(spriteFrameName, space);
+    }
+}
